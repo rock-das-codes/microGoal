27e3d89a61189d74183589cc61a73c099e61b0a3
rock-das-codes <93854139+rock-das-codes@users.noreply.github.com>
2025-11-03 09:30:00 +0000

    little fix of app crash

--- microgoal/app/_layout.tsx
+++ microgoal/app/_layout.tsx
@@ -1,33 +1,33 @@
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect, useState } from "react";
-import { SplashScreen, Stack, router } from "expo-router";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { useEffect, useState } from "react";
+// import { SplashScreen, Stack, router } from "expo-router";
 
-export default function RootLayout() {
-    const [ready, setReady] = useState(false);
-  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
-    useEffect(() => {
-    const checkOnboarding = async () => {
-      const hasSeen = await AsyncStorage.getItem("hasSeenOnboarding");
-      setIsFirstLaunch(!hasSeen);
-      setReady(true);
-    };
-    checkOnboarding();
-  }, []);
+// export default function RootLayout() {
+//     const [ready, setReady] = useState(false);
+//   const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
+//     useEffect(() => {
+//     const checkOnboarding = async () => {
+//       const hasSeen = await AsyncStorage.getItem("hasSeenOnboarding");
+//       setIsFirstLaunch(!hasSeen);
+//       setReady(true);
+//     };
+//     checkOnboarding();
+//   }, []);
 
  
-useEffect(() => {
-    if (ready) {
-      if(isFirstLaunch){
-        router.replace("/(onboardings)");
-      }
-      else{
-        router.replace("/(tabs)/home");
-      }
-    }
-},[ready]);
+// useEffect(() => {
+//     if (ready) {
+//       if(isFirstLaunch){
+//         router.replace("/(onboardings)");
+//       }
+//       else{
+//         router.replace("/(tabs)/home");
+//       }
+//     }
+// },[ready]);
   
-  return <Stack  screenOptions={{headerShown: false}}>
+//   return <Stack  screenOptions={{headerShown: false}}>
 
-  </Stack>
-}
+//   </Stack>
+// }
